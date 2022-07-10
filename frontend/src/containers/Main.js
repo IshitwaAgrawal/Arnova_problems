084bdbe6fd9c87e8b471041ada7610ca7e6c015a
IshitwaAgrawal <ishitwa2000@gmail.com>
2022-07-10 14:14:30 +0530

    initialized project

---
+++ frontend/src/containers/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div className="header"> Hello !</div>
+    );
+}
+
+export default Main;
---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./containers/Main";
+import "./style.css";
+ReactDOM.render(
+      <Main />,
+  document.querySelector("#root")
+);
